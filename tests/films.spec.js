4d4e74ce34c67f6168b01f547775ab521320d690
raquelEve <calculadoraverde@gmail.com>
2024-04-04 21:24:36 +0200

    s3 ex6: test moviesAverageByCategory

--- tests/films.spec.js
+++ tests/films.spec.js
@@ -314,7 +314,35 @@ describe('Function "moviesAverageByCategory"', () => {
   it('should be zero', () => {
     expect(moviesAverageByCategory(movies, '')).toBe(0);
   });
-});
+  // calculating average rate
+  const arrTest = [{
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  },
+  {
+    title: 'The Dark Knight',
+    year: 2008,
+    director: 'Christopher Nolan',
+    duration: '2h 32min',
+    genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+    score: 9
+  },
+  {
+    title: '12 Angry Men',
+    year: 1957,
+    director: 'Sidney Lumet',
+    duration: '1h 36min',
+    genre: ['Drama'],
+    score: 8.9
+    }]
+  it('calculating average rate with two decimals', () => {
+    expect(moviesAverageByCategory(arrTest, 'Crime')).toBe(9.00);
+  }
+);
 
 // Exercise 7
 describe('Function "hoursToMinutes"', () => {
